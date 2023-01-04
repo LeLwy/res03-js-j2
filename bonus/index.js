38c5792fbe5810ff0701a2ef4d3343fa3e9ce63a
LeLwy <louis.chancioux@gmail.com>
2023-01-04 16:26:32 +0000

    Exo bonus

---
+++ bonus/index.js
@@ -0,0 +1,18 @@
+function sortNumbers(array){
+    
+    let arraySorted = [];
+    
+    for(element of array){
+        
+        for( let i =0; i < array.length; i++){
+            
+            
+        }
+    }
+    
+    return arraySorted;
+}
+
+let numbers = [-23,42,21,90,54,362,-12,57,78,84,35,546,128,7,15,65,28];
+
+console.log(sortNumbers(numbers));
